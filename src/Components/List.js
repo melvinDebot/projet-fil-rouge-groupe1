701072963508adff9af79fdac5ee5502321a45d6
edenbergel <eden.bergel@hetic.net>
2020-02-11 15:07:16 +0100

    assets

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import imgOne from './assets/imgOne.png'
+
 
 function List(props){
   return(
     <div className="box">
       <div className="box--img">
+        <img src={imgOne} alt="" />
         <div className="block--img">{props.meter}</div>
       </div>
       <div className="box--text">
